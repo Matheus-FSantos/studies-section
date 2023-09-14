d0017d89e48bea9d1271b2356334141907905910
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2023-09-14 11:58:00 -0300

    feat: add new component 'RepositoryList.jsx'

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,54 @@
+export default function RepositoryList() {
+    const repositoryName = "unform";
+
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
